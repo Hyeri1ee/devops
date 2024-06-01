d42c9937e2fe6138aaa33cff304b774339259837
Hyeri1ee <haerizian10@gmail.com>
2024-06-01 16:48:35 +0200

    [fix] recent

--- dockerize.sh
+++ dockerize.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
-#
-# Usage:
-#  build - Create docker container(s) for the application
-#  run   - Run the application as container(s)
-#  stop  - Stop the container(s)
 
-# TODO: Please write a working bash script here
+build_containers() {
+    echo "Building Docker containers..."
+    docker-compose build
+}
+
+run_containers() {
+    echo "Starting Docker containers..."
+    docker-compose up -d
+}
+
+stop_containers() {
+    echo "Stopping Docker containers..."
+    docker-compose down
+}
+
+case "$1" in
+    build)
+        build_containers
+        ;;
+    run)
+        run_containers
+        ;;
+    stop)
+        stop_containers
+        ;;
+    *)
+        echo "Usage: $0 {build|run|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
