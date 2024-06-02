16cefaf44825862a8058b13875b7b9faa694e369
FKryleckiSE <560567@student.saxion.nl>
2024-06-02 14:59:28 +0200

    changes to the project

---
+++ infra/user_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo su -
+sudo apt-get update
+sudo apt install -y docker.io
+sudo apt-get install -y git
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl start docker
+sudo systemctl enable docker
+git clone -b point3 --single-branch https://FKryleckiSE:glpat-Qj-yoJjFz_hUKKZ_Sek_@gitlab.com/saxion.nl/hbo-ict/2.3-devops/2023-2024/exam-retake/03.git
+cd 03
+sudo docker-compose up -d
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,17 @@
-# Your variables here
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+  default     = "password123"
+}
+
+variable "db_name" {
+  description = "The database name for the RDS instance"
+  type        = string
+  default     = "mydatabase"
+}
